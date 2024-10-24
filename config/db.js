a740bacdd369c360e1bd93ad673d58cd65653a44
FlimFli <peckbboy2@gmail.com>
2024-10-24 12:25:06 +0700

    first commit

---
+++ config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export default async function connentMondoDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_DB_URI)
+        console.log("Connected to MongoDB")
+    } catch (error) {
+        console.log(error)       
+    }
+    
+     
+}
+    
